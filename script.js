ddd91ee230045ea499ff35f5826436e6027ba8af
datalal <lawsonw2@gmail.com>
2017-01-31 10:47:34 -0500

    sidebar update
    jquery fix

---
+++ script.js
@@ -0,0 +1,23 @@
+var $win = $(window),
+    w = 0,h = 0,
+    rgb = [],
+    getWidth = function() {
+        w = $win.width();
+        h = $win.height();
+    };
+
+$win.resize(getWidth).mousemove(function(e) {
+
+    rgb = [
+        Math.round(e.pageX/w * 255),
+        Math.round(e.pageY/h * 255),
+        150
+
+
+    ];
+
+    $('#sidebar').css('background','rgb('+rgb.join(',')+')');
+    $('#rightbox').css('background','rgb('+rgb.join(',')+')');
+
+
+}).resize();
