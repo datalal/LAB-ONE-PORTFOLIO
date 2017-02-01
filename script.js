d83797850cdd35bab4596721f84733dd04810906
datalal <lawsonw2@gmail.com>
2017-02-01 09:33:30 -0500

    color bar fix
    moved color bar / removed text if

--- script.js
+++ script.js
@@ -32,13 +32,13 @@ $(document).mousemove(function(e){
     var $height = ($(document).height())/255;
     var $pageX = parseInt(e.pageX / $width,10);
     var $pageY = parseInt(e.pageY / $height,10);
-    $(".sidebar").css("background-color", "rgb("+$pageX+","+$pageY+",150)");
+    $(".bottombar").css("background-color", "rgb("+$pageX+","+$pageY+",150)");
     $(".rightbox").css("background-color", "rgba("+$pageX+","+$pageY+","+$pageX+", 0.3)");
     $(".rightbox2").css("background-color", "rgba("+$pageX+","+$pageY+",90, 0.5)");
     $(".rightbox3").css("background-color", "rgba("+$pageX+","+$pageY+",40, 0.7)");
     $(".rightbox4").css("background-color", "rgba("+$pageX+","+$pageY+",10, 0.8)");
 
-    if ($pageX < 60 || $pageY < 70) {
+/*    if ($pageX < 60 || $pageY < 70) {
   $(".navlink").css("color", "white");
   $(".navlink").css("background-color", "grey");
 
@@ -47,5 +47,5 @@ $(document).mousemove(function(e){
        $(".navlink").css("background-color", "transparent");
 
 }
-
+*/
 });
